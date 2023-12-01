026da4ebe69527db6bf8b49750c414f574a8f520
hd2yao <1054290355@qq.com>
2023-12-01 16:30:58 +0800

    feat(etcd):use etcd to manager collect config

--- etcd_demo/main.go
+++ etcd_demo/main.go
@@ -1,45 +1,46 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"time"
+    "context"
+    "fmt"
+    "time"
 
-	"go.etcd.io/etcd/client/v3"
+    "go.etcd.io/etcd/client/v3"
 )
 
 // 代码连接 etcd
 
 func main() {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"127.0.0.1:2379"},
-		DialTimeout: time.Second * 5,
-	})
-	if err != nil {
-		fmt.Printf("connect to etcd failed, err:%v", err)
-		return
-	}
-	defer cli.Close()
+    cli, err := clientv3.New(clientv3.Config{
+        Endpoints:   []string{"127.0.0.1:2379"},
+        DialTimeout: time.Second * 5,
+    })
+    if err != nil {
+        fmt.Printf("connect to etcd failed, err:%v", err)
+        return
+    }
+    defer cli.Close()
 
-	// put
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	_, err = cli.Put(ctx, "sy", "my_love")
-	if err != nil {
-		fmt.Printf("put to etcd failed, err:%v", err)
-		return
-	}
-	cancel()
+    // put
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    valueStr := `[{"path":"D:/Program/project/go/log-agent/logs/sy.log","topic":"sy"},{"path":"D:/Program/project/go/log-agent/logs/web_log.log","topic":"web_log"}]`
+    _, err = cli.Put(ctx, "collect_log_conf", valueStr)
+    if err != nil {
+        fmt.Printf("put to etcd failed, err:%v", err)
+        return
+    }
+    cancel()
 
-	// get
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	gr, err := cli.Get(ctx, "sy")
-	if err != nil {
-		fmt.Printf("get from etcd failed, err:%v", err)
-		return
-	}
-	for _, ev := range gr.Kvs {
-		fmt.Printf("key:%s value:%s\n", ev.Key, ev.Value)
-	}
-	cancel()
+    // get
+    ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+    gr, err := cli.Get(ctx, "collect_log_conf")
+    if err != nil {
+        fmt.Printf("get from etcd failed, err:%v", err)
+        return
+    }
+    for _, ev := range gr.Kvs {
+        fmt.Printf("key:%s value:%s\n", ev.Key, ev.Value)
+    }
+    cancel()
 
 }
