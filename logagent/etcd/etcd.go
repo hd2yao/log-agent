1bf6ff31a5040f5bb06ca0b6ed11405542cdb7a3
hd2yao <1054290355@qq.com>
2023-12-05 15:59:59 +0800

    feat(etcd):add etcd del operation

--- logagent/etcd/etcd.go
+++ logagent/etcd/etcd.go
@@ -4,73 +4,79 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/hd2yao/log-agent/logagent/tailf"
 	"time"
 
 	"github.com/sirupsen/logrus"
 	"go.etcd.io/etcd/client/v3"
 
 	"github.com/hd2yao/log-agent/logagent/common"
+	"github.com/hd2yao/log-agent/logagent/tailf"
 )
 
 // etcd
 
 var (
-	client *clientv3.Client
+    client *clientv3.Client
 )
 
 func Init(address []string) (err error) {
-	client, err = clientv3.New(clientv3.Config{
-		Endpoints:   address,
-		DialTimeout: time.Second * 5,
-	})
-	if err != nil {
-		fmt.Printf("connect to etcd failed, err:%v\n", err)
-		return
-	}
-	return
+    client, err = clientv3.New(clientv3.Config{
+        Endpoints:   address,
+        DialTimeout: time.Second * 5,
+    })
+    if err != nil {
+        fmt.Printf("connect to etcd failed, err:%v\n", err)
+        return
+    }
+    return
 }
 
 // GetConf 拉取日志收集配置项的函数
 func GetConf(key string) (collectEntryList []common.CollectEntry, err error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
-	defer cancel()
-	resp, err := client.Get(ctx, key)
-	if err != nil {
-		logrus.Errorf("get conf from etcd by key:%s failed, err:%v\n", key, err)
-		return
-	}
-	if len(resp.Kvs) == 0 {
-		logrus.Warningf("get len:0 conf from etcd by key:%s\n", key)
-		return
-	}
-	ret := resp.Kvs[0]
-	// ret.Value // json 格式字符串
-	err = json.Unmarshal(ret.Value, &collectEntryList)
-	if err != nil {
-		logrus.Errorf("json unmarshal failed, err:%v", err)
-		return
-	}
-	return
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+    defer cancel()
+    resp, err := client.Get(ctx, key)
+    if err != nil {
+        logrus.Errorf("get conf from etcd by key:%s failed, err:%v\n", key, err)
+        return
+    }
+    if len(resp.Kvs) == 0 {
+        logrus.Warningf("get len:0 conf from etcd by key:%s\n", key)
+        return
+    }
+    ret := resp.Kvs[0]
+    // ret.Value // json 格式字符串
+    err = json.Unmarshal(ret.Value, &collectEntryList)
+    if err != nil {
+        logrus.Errorf("json unmarshal failed, err:%v", err)
+        return
+    }
+    return
 }
 
 // WatchConf 监控 etcd 中日志收集项配置变化的函数
 func WatchConf(key string) {
-	for {
-		watchChan := client.Watch(context.Background(), key)
-		var newConf []common.CollectEntry
-		for wresp := range watchChan {
-			logrus.Info("get new conf from etcd!")
-			for _, evt := range wresp.Events {
-				fmt.Printf("type:%s key:%s value:%s\n", evt.Type, evt.Kv.Key, evt.Kv.Value)
-				err := json.Unmarshal(evt.Kv.Value, &newConf)
-				if err != nil {
-					logrus.Errorf("json unmarshal new conf failed, err:%v", err)
-					continue
-				}
-				// 告诉tailfile这个模块应该启用新的配置了!
-				tailf.SendNewConf(newConf) // 没有任何接收就是阻塞的
-			}
-		}
-	}
+    for {
+        watchChan := client.Watch(context.Background(), key)
+        for wresp := range watchChan {
+            logrus.Info("get new conf from etcd!")
+            for _, evt := range wresp.Events {
+                fmt.Printf("type:%s key:%s value:%s\n", evt.Type, evt.Kv.Key, evt.Kv.Value)
+                var newConf []common.CollectEntry
+                if evt.Type == clientv3.EventTypeDelete {
+                    // 如果是删除
+                    logrus.Warning("FBI warning:etcd delete the key!!!")
+                    tailf.SendNewConf(newConf) // 没有任何接收就是阻塞的
+                    continue
+                }
+                err := json.Unmarshal(evt.Kv.Value, &newConf)
+                if err != nil {
+                    logrus.Errorf("json unmarshal new conf failed, err:%v", err)
+                    continue
+                }
+                // 告诉tailfile这个模块应该启用新的配置了!
+                tailf.SendNewConf(newConf) // 没有任何接收就是阻塞的
+            }
+        }
+    }
 }
