0302d38c16076cc3e03c063763c088c179a6eacc
hd2yao <1054290355@qq.com>
2023-12-06 15:13:00 +0800

    feat(common):add get IP func

--- logagent/common/common.go
+++ logagent/common/common.go
@@ -1,7 +1,31 @@
 package common
 
+import (
+    "fmt"
+    "net"
+    "strings"
+)
+
+const (
+    CanNotGetIP = "can not get IP"
+)
+
 // CollectEntry 要收集的日志的配置项的结构体
 type CollectEntry struct {
     Path  string `json:"path"`
     Topic string `json:"topic"`
+}
+
+// GetOutboundIP 获取本机 IP
+func GetOutboundIP() (ip string, err error) {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        return CanNotGetIP, nil
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+    fmt.Println(localAddr.IP.String())
+    ip = strings.Split(localAddr.IP.String(), ":")[0]
+    return
 }
